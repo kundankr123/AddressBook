38950c6ab68f70886e6221739c79682bf046c262
kundan <goswamikundan123@gmail.com>
2023-02-21 17:23:34 +0530

    Adding files which create an Address book and add contact to it.

--- Program.cs
+++ Program.cs
@@ -5,6 +5,13 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program. ");
+
+
+            AddressBook myAddressBook = new AddressBook();
+            Contacts newContact = new Contacts("Kundan", "kumar", "INDIA", "7975461361", "kundankr@example.com");
+
+            myAddressBook.AddContact(newContact);
+            myAddressBook.PrintContacts();
         }
     }
 }
