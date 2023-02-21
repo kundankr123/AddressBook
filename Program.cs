d7a4f46ade9d04fc02bb7187b7b02a8391af969b
kundan <goswamikundan123@gmail.com>
2023-02-21 17:34:26 +0530

    Adding file which reads the contact details from console then add it to address book.

--- Program.cs
+++ Program.cs
@@ -6,11 +6,34 @@
         {
             Console.WriteLine("Welcome to Address Book Program. ");
 
+            //UseCase1
+
+            //AddressBook myAddressBook = new AddressBook();
+            //Contacts newContact = new Contacts("Kundan", "kumar", "INDIA", "7975461361", "kundankr@example.com");
+            //myAddressBook.AddContact(newContact);
+            //myAddressBook.PrintContacts();
+
+
+            //UseCase2
 
             AddressBook myAddressBook = new AddressBook();
-            Contacts newContact = new Contacts("Kundan", "kumar", "INDIA", "7975461361", "kundankr@example.com");
+
+            Console.WriteLine("Add new contact:");
+            Console.Write("Enter first name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Enter last name: ");
+            string lastName = Console.ReadLine();
+            Console.Write("Enter country: ");
+            string country = Console.ReadLine();
+            Console.Write("Enter phone number: ");
+            string phoneNumber = Console.ReadLine();
+            Console.Write("Enter email: ");
+            string email = Console.ReadLine();
+
+            Contacts newContact = new Contacts(firstName, lastName, country, phoneNumber, email);
 
             myAddressBook.AddContact(newContact);
+            Console.WriteLine("Contact Details: ");
             myAddressBook.PrintContacts();
         }
     }
